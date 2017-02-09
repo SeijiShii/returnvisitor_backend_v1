a4e51213e7e856a86c6b46164248f05daf698b0f
Seiji Shii <seiji.shii@gmail.com>
2017-02-09 16:50:57 +0900

    rv_http_server > doPost > doPostUser テスト済

--- rv_http_server.js
+++ rv_http_server.js
@@ -3,50 +3,88 @@ var url = require('url');
 var port = 1337;
 
 var dbclient = require('./dbclient');
-
-var pathArray, query;
-var users = require('./returnvisitor_users');
+var ReturnVisitorUsers = require('./returnvisitor_users');
+var users = new ReturnVisitorUsers(dbclient);
 
 var server = http.createServer(function(req, res){
 
+  // メソッドで振り分ける
+  switch (req.method) {
+    case 'POST':
+        doPost(req, res);
+      break;
+    default:
+      res.writeHead(404, {'Content-type': 'text/plain'});
+      res.end('Not Found: Not yet implemented method: ' + req.method);
+      break;
+  }
+
+
+});
+
+var doPost = function(req, res) {
+  console.log('doPost called');
+
   var reqUrl = req.url;
-  console.log(reqUrl);
+  // console.log(reqUrl);
 
   var path = url.parse(reqUrl);
-  console.dir(path);
+  // console.dir(path);
 
   var pathname = path.pathname;
-  console.log('pathname: ' + pathname);
-
-  var pathArray = pathname.split('/');
+  // console.log('pathname: ' + pathname);
 
   // pathArrayの空白要素を削除
-  var tmpArray = [];
-  for (var i = 0 ; i < pathArray.length; ++i){
-    if (pathArray[i] !== '') {
-      tmpArray.push(pathArray[i]);
+  var tmpArray = pathname.split('/');
+  var pathArray = [];
+  for (var i = 0 ; i < tmpArray.length; ++i){
+    if (tmpArray[i] !== '') {
+      pathArray.push(tmpArray[i]);
     }
   }
-  pathArray = [];
-  for (var i = 0 ; i < tmpArray.length ; ++i) {
-    pathArray.push(tmpArray[i]);
-  }
-  console.dir(pathArray);
+  // console.dir(pathArray);
 
+  // queryを抽出
   var query = reqUrl.query;
 
   // リソースで振り分ける
   var resourceName = pathArray.shift();
-  console.log(resourceName);
+  // console.log(resourceName);
   switch (resourceName) {
-    // case 'users':
-    //
-    //   break;
+    case 'users':
+      doPostUser(req, res);
+      break;
     default:
       res.writeHead(404, {'Content-type': 'text/plain'});
       res.end('Not Found: Not yet implemented resource: ' + resourceName);
   }
-});
+}
+
+var doPostUser = function(req, res) {
+  // bodyをゲット
+  var body = [];
+  req.on('data', function(chunk){
+    body.push(chunk);
+  }).on('end', function(){
+    body = Buffer.concat(body).toString();
+    // console.log('body: ' + body);
+    var user = JSON.parse(body);
+    // console.dir(user);
+    // console.log('user.user_name: ' + user.user_name);
+    // console.log('user.password: ' + user.password);
+
+    users.postUser(user.user_name, user.password, function(data, err){
+
+      console.log('data in postUser callback: ');
+      console.dir(data);
+
+      res.writeHead(200, {'Content-type': 'application/json'})
+      var jString = JSON.stringify(data);
+      console.log('jString: ' + jString);
+      res.end(jString);
+    });
+  });
+}
 
 server.listen(port, function(){
   console.log('Server listening on: ' + port);
---
+++ rv_http_test_clients/rv_http_test_client_post_user.js
@@ -0,0 +1,42 @@
+var request = require('request');
+var should = require('should');
+
+var options = {
+  method: 'POST',
+  url: 'http://localhost:1337/users',
+  json: true,
+  headers: {
+    'Content-type': 'application/json'
+  },
+  body: {user_name: 'seijishii',
+          password: 'hogehoge'}
+};
+
+request(options, function (error, response, body) {
+
+  console.log('body: ' + body);
+  // console.dir(error);
+  // console.dir(response);
+
+  var user = body;
+
+  try{
+    user.user_name.should.equal('seijishii');
+  } catch(err){
+    console.log('--- AssertionError ---');
+    console.log(err);
+    console.log('--- stack trace ---');
+    console.log(err.stack);
+  }
+
+  try{
+    user.password.should.equal('hogehoge');
+  } catch(err){
+    console.log('--- AssertionError ---');
+    console.log(err);
+    console.log('--- stack trace ---');
+    console.log(err.stack);
+  }
+
+
+})
