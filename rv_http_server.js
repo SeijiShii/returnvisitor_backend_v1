ff90a84561f5c503a43e8a72fb0818eb36728762
Seiji Shii <seiji.shii@gmail.com>
2017-02-09 18:10:52 +0900

    rv_http_server > doGet > doGetUser テスト済

--- rv_http_server.js
+++ rv_http_server.js
@@ -8,10 +8,26 @@ var users = new ReturnVisitorUsers(dbclient);
 
 var server = http.createServer(function(req, res){
 
+  var reqUrl = req.url;
+  // queryをオブジェクトでtrue
+  var path = url.parse(reqUrl, true);
+  var pathname = path.pathname;
+  var tmpArray = pathname.split('/');
+  var pathArray = [];
+  for (var i = 0 ; i < tmpArray.length; ++i){
+    if (tmpArray[i] !== '') {
+      pathArray.push(tmpArray[i]);
+    }
+  }
+  var query = path.query;
+
   // メソッドで振り分ける
   switch (req.method) {
+    case 'GET':
+      doGet(req, res, pathArray, query);
+      break;
     case 'POST':
-        doPost(req, res);
+      doPost(req, res, pathArray);
       break;
     default:
       res.writeHead(404, {'Content-type': 'text/plain'});
@@ -22,41 +38,56 @@ var server = http.createServer(function(req, res){
 
 });
 
-var doPost = function(req, res) {
-  console.log('doPost called');
-
-  var reqUrl = req.url;
-  // console.log(reqUrl);
-
-  var path = url.parse(reqUrl);
-  // console.dir(path);
-
-  var pathname = path.pathname;
-  // console.log('pathname: ' + pathname);
-
-  // pathArrayの空白要素を削除
-  var tmpArray = pathname.split('/');
-  var pathArray = [];
-  for (var i = 0 ; i < tmpArray.length; ++i){
-    if (tmpArray[i] !== '') {
-      pathArray.push(tmpArray[i]);
-    }
-  }
-  // console.dir(pathArray);
-
-  // queryを抽出
-  var query = reqUrl.query;
+var doGet = function(req, res, pathArray, query) {
+  // console.log('doGet called');
 
   // リソースで振り分ける
-  var resourceName = pathArray.shift();
+  var resourceRootName = pathArray.shift();
   // console.log(resourceName);
-  switch (resourceName) {
+  switch (resourceRootName) {
+    case 'users':
+      doGetUser(req, res, pathArray, query);
+      break;
+    default:
+      res.writeHead(404, {'Content-type': 'text/plain'});
+      res.end('Not Found: Not yet implemented resource: ' + resourceRootName + ' in GET method.');
+  }
+}
+
+var doGetUser = function(req, res, pathArray, query) {
+
+  var user_name = pathArray.shift();
+  console.log('user_name: ' + user_name);
+
+  console.dir(query);
+
+  var password = query.password;
+  console.log('password: ' + password);
+
+  users.getUser(user_name, password, function(data, err){
+    console.log('data in getUser callback: ');
+    console.dir(data);
+
+    res.writeHead(200, {'Content-type': 'application/json'})
+    var jsonString = JSON.stringify(data);
+    console.log('jsonString: ' + jsonString);
+    res.end(jsonString);
+  });
+}
+
+var doPost = function(req, res, pathArray) {
+  // console.log('doPost called');
+
+  // リソースで振り分ける
+  var resourceRootName = pathArray.shift();
+  // console.log(resourceName);
+  switch (resourceRootName) {
     case 'users':
       doPostUser(req, res);
       break;
     default:
       res.writeHead(404, {'Content-type': 'text/plain'});
-      res.end('Not Found: Not yet implemented resource: ' + resourceName);
+      res.end('Not Found: Not yet implemented resource: ' + resourceRootName + ' in POST method.');
   }
 }
 
@@ -79,9 +110,9 @@ var doPostUser = function(req, res) {
       console.dir(data);
 
       res.writeHead(200, {'Content-type': 'application/json'})
-      var jString = JSON.stringify(data);
-      console.log('jString: ' + jString);
-      res.end(jString);
+      var jsonString = JSON.stringify(data);
+      console.log('jsonString: ' + jsonString);
+      res.end(jsonString);
     });
   });
 }
---
+++ rv_http_test_clients/rv_http_test_client_get_users.js
@@ -0,0 +1,44 @@
+var request = require('request');
+var options = {
+  method: 'GET',
+  url: 'http://localhost:1337/users/seijishii?password=hogehoge',
+  json: true
+};
+
+var assertBodyData = function(user) {
+  try{
+    user.user_name.should.equal('seijishii')
+  } catch(err){
+    console.log('--- AssertionError ---');
+    console.log(err);
+    console.log('--- stack trace ---');
+    console.log(err.stack);
+  }
+
+  try{
+    user.password.should.equal('hogehoge')
+  } catch(err){
+    console.log('--- AssertionError ---');
+    console.log(err);
+    console.log('--- stack trace ---');
+    console.log(err.stack);
+  }
+}
+
+request(options, function (error, response, body) {
+
+  console.log('statusCode: ' + response.statusCode);
+  console.dir(body);
+
+  assertBodyData
+
+  // try{
+  //   response.statusCode.should.equal('200');
+  //   assertBodyData(body);
+  // } catch(err){
+  //   console.log('--- AssertionError ---');
+  //   console.log(err);
+  //   console.log('--- stack trace ---');
+  //   console.log(err.stack);
+  // }
+})
